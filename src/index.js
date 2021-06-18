a10df33a9fa643ca3e145289dfee0db418c21ea6
Grimille <mickael.dhainaut@gmail.com>
2021-06-18 21:32:55 +0200

    🛂  Get the JWT with user credentials

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 const fetch = require('node-fetch');
+const dotenv = require('dotenv');
 
-// https://duolingo.com/login 
+dotenv.config();
 
+const LOGIN_URL =  'https://www.duolingo.com/2017-06-30/login';
+const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
+
+fetch(LOGIN_URL, {
+    method: 'post',
+    body: JSON.stringify({
+        identifier: process.env.DUOLINGO_USERNAME,
+        password: process.env.DUOLINGO_PASSWORD
+    }),
+    headers: { 'User-Agent': USER_AGENT },
+}).then(res => {
+    console.log(res.headers.get('jwt'));
+    res.json();
+}).then();
