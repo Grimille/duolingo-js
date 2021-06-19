c9c45604e3020c4741568bc7e28204e8ac6fd309
Grimille <mickael.dhainaut@gmail.com>
2021-06-19 11:47:24 +0200

    🔥  Removed callback, so 2011

--- src/requester.js
+++ src/requester.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
 
-const send = async (url, data, callback) => {
+const send = async (url, data) => {
     return fetch(url, {
         method: data != null ? 'post' : 'get',
         body: JSON.stringify(data),
