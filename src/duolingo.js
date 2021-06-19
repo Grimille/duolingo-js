03b56dafaa9a28153af1f19996966bba0da1ad3b
Grimille <mickael.dhainaut@gmail.com>
2021-06-19 11:46:44 +0200

    ⚡️  Made login async

--- src/duolingo.js
+++ src/duolingo.js
@@ -5,14 +5,26 @@ class Duolingo {
     constructor(identifier, password) {
         this.identifier = identifier;
         this.password = password;
+        this.jwt = null;
     }
 
-    login() {
-        console.log('id', this.identifier);
-        requester.send(LOGIN_URL, {
-            identifier: this.identifier,
-            password: this.password
-        });
+    async login() {
+        console.log('Login into Duolingo using credentials...');
+
+        if(this.jwt === null) {
+            await requester.send(LOGIN_URL, {
+                identifier: this.identifier,
+                password: this.password
+            }).then(response => {
+                this.jwt = response.headers.get('jwt');
+            }).catch(error => {
+                console.error(error);
+            });
+        } else {
+            console.log('User is already logged in. Using JWT...')
+        }
+
+        return this.jwt !== null;
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -6,4 +6,7 @@ dotenv.config();
 const Duolingo = require('../src/duolingo');
 const client = new Duolingo(process.env.DUOLINGO_IDENTIFIER, process.env.DUOLINGO_PASSWORD);
 
-client.login();
+(async () => {
+    await client.login();
+})();
+
