3c34067cd0fd365c22d7efbb3bee38a8b7721da0
Grimille <mickael.dhainaut@gmail.com>
2021-06-19 12:04:35 +0200

    🎨  Added JWT in headers

--- src/requester.js
+++ src/requester.js
@@ -1,11 +1,14 @@
 const fetch = require('node-fetch');
 const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36';
 
-const send = async (url, data) => {
+const send = async (url, data, jwt) => {
     return fetch(url, {
         method: data != null ? 'post' : 'get',
         body: JSON.stringify(data),
-        headers: { 'User-Agent': USER_AGENT },
+        headers: { 
+            'User-Agent': USER_AGENT,
+            'Authorization' : 'Bearer ' + jwt 
+        },
     });
 }
 
