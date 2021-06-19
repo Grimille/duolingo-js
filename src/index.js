284c41c02342e9e2c870df3766f3586f617ba235
Grimille <mickael.dhainaut@gmail.com>
2021-06-19 12:58:42 +0200

    🔥  Removed testing code in order to publish it

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 const dotenv = require('dotenv');
 
 dotenv.config();
-
-// -- Duolingo -- //
-const Duolingo = require('../src/duolingo');
-const client = new Duolingo(process.env.DUOLINGO_IDENTIFIER, process.env.DUOLINGO_PASSWORD);
-
-(async () => {
-    await client.login();
-    
-    const grimille = await client.getUserData("Grimille");
-    const aleroyke = await client.getUserData('aleroyke');
-
-    console.log(aleroyke);
-})();
-
