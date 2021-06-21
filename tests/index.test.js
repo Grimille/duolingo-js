923d0832967a1fe55d34f9b99f742e18208d3ef6
Grimille <mickael.dhainaut@gmail.com>
2021-06-21 13:58:03 +0200

    ✅  Implemented some token testing

---
+++ tests/index.test.js
@@ -0,0 +1,20 @@
+const { Duolingo } = require('../src/index');
+const { readTokenFromFile, tokenFileExist } = require('../src/token');
+
+test('duolingo.login', async () => {
+    const client = new Duolingo('RandomUser978392023ab2', 'RandomPassword93827475827ab3');
+    const loggedIn = await client.login();
+
+    expect(loggedIn).toBe(false);
+});
+
+test('duolingo.existToken', () => {
+    expect(tokenFileExist('tests/duolingo/test.json')).toBe(false);
+    expect(tokenFileExist('tests/duolingo/token.json')).toBe(true);
+});
+
+test('duolingo.readToken', () => {
+    const token = readTokenFromFile('tests/duolingo/token.json');
+
+    expect(token).toMatch('');
+});
