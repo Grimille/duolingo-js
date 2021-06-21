63255de68af029db740fab24837b0120e3785a16
Grimille <mickael.dhainaut@gmail.com>
2021-06-21 15:55:02 +0200

    🐛  Fixed GET method with body

--- src/requester.js
+++ src/requester.js
@@ -2,14 +2,19 @@ const fetch = require('node-fetch');
 const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36';
 
 const send = async (url, data, jwt) => {
-    return fetch(url, {
-        method: data != null ? 'post' : 'get',
-        body: JSON.stringify(data),
+    const params = {
+        method: data !== null ? 'post' : 'get',
         headers: { 
             'User-Agent': USER_AGENT,
             'Authorization' : 'Bearer ' + jwt 
         },
-    });
+    };
+
+    if(params.method === 'post') {
+        params.body = JSON.stringify(data);
+    }
+
+    return fetch(url, params);
 }
 
 exports.send = send;
